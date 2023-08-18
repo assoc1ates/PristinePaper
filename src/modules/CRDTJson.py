6349e950524833692d220dcaab3a555cc485d5d3
dOsan3 <>
2023-08-18 15:20:08 -0400

    Retain existing UUIDs for duplicate fields in merged.json - Prevent updates to UUID and timestamp of existing fields - Ensure UUID consistency between runs for existing fields

--- src/modules/CRDTJson.py
+++ src/modules/CRDTJson.py
@@ -14,6 +14,10 @@ class CRDTJson:
         timestamp = time.time()
 
         with self.lock:
+            # Check if the replaced_text is already in the data
+            if replaced_text in self.data:
+                # If it is, we simply return and do not update it
+                return
             if (replaced_text in self.timestamps and self.timestamps[replaced_text] > timestamp) or (process_uuid in self.uuids):
                 return
 
