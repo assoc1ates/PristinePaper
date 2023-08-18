25295a9b4798d6b59d1557215381937f3dde6954
dOsan3 <>
2023-08-18 15:55:46 -0400

    Integrate Replicate API and secure API token storage

--- src/Process1.py
+++ src/Process1.py
@@ -1,12 +1,12 @@
-# Process1.py
+# Process1.py`
 import sys
-import uuid  # <-- Import the uuid module here
-
+import uuid
 from modules.CRDTJson import CRDTJson
+from modules.LlamaReplicateAPI import LlamaReplicateAPI  # <-- Import the LlamaReplicateAPI module here
 import json
 
 def main():
-    process1_uuid = str(uuid.uuid4())  # <-- Generate a UUID for the process
+    process1_uuid = str(uuid.uuid4())
     process1_json = CRDTJson()
     process1_json.set("Hello", process1_uuid)
 
@@ -14,6 +14,11 @@ def main():
         json.dump(process1_json.data, file)
 
     process1_json.save_metadata('output-files/process1_metadata.json')
+
+    # Use LlamaReplicateAPI here
+    llama_api = LlamaReplicateAPI()
+    for item in llama_api.run_model("Hello, how are you?", temperature=0.75):
+        print(item)
 
     with open('output-files/process1.done', 'w') as file:
         file.write('Done')
